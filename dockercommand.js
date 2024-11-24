f2ca8f81a53103447df7da67f5ce78644a59b7df
Ahsan Hafeez Satti <ahsansatti402@gmail.com>
2024-11-24 17:01:44 +0500

    Add dockercommand.js to .gitignore and remove it from tracking

--- dockercommand.js
+++
@@ -1,7 +0,0 @@
-// 1) Start Zookeper Container and expose PORT 2181.
-// cmd 1
-// docker run -p 2181:2181 zookeeper
-
-// 2) Start Kafka Container, expose PORT 9092 and setup ENV variables.
-// cmd 2
-// docker run -p 9092:9092 -e KAFKA_ZOOKEEPER_CONNECT=192.168.100.15:2181 -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://192.168.100.15:9092 -e KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR=1 confluentinc/cp-kafka
